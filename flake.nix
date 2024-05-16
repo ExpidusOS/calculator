e9465271815b9b46de248242f1303562dd8e1612
Tristan Ross <tristan.ross@midstall.com>
2024-05-15 18:34:12 -0700

    feat: update to flutter v3.22.0

--- flake.nix
+++ flake.nix
@@ -9,70 +9,96 @@
     http2 = false;
   };
 
-  inputs.expidus-sdk = {
-    url = github:ExpidusOS/sdk;
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flutter-v322.url = "github:ExpidusOS/nixpkgs/feat/flutter-3.22.0";
   };
 
-  inputs.nixpkgs.url = github:ExpidusOS/nixpkgs;
-
-  outputs = { self, expidus-sdk, nixpkgs }:
-    with expidus-sdk.lib;
-    flake-utils.eachSystem flake-utils.allSystems (system:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    flutter-v322,
+    ...
+  }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = expidus-sdk.legacyPackages.${system};
-        deps = builtins.fromJSON (readFile ./deps.json);
-        shortRev = self.shortRev or (substring 7 7 fakeHash);
-        shortRevCodes = map strings.charToInt (stringToCharacters shortRev);
-        buildCode = foldr (a: b: "${toString a}${toString b}") "" shortRevCodes;
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) lib;
 
-        shortVersion = builtins.elemAt (splitString "+" (builtins.elemAt deps 0).version) 0;
+        shortRev = self.shortRev or (lib.substring 7 7 lib.fakeHash);
+        shortRevCodes = map lib.strings.charToInt (lib.stringToCharacters shortRev);
+        buildCode = lib.foldr (a: b: "${toString a}${toString b}") "" shortRevCodes;
+
+        shortVersion = "0.2.0";
         version = "${shortVersion}+${buildCode}";
-      in {
-        packages.default = pkgs.flutter.buildFlutterApplication {
-          pname = "expidus-calculator";
-          version = "${shortVersion}+git-${shortRev}";
 
-          src = cleanSource self;
+        overlay = f: p: {
+          expidus = p.expidus // {
+            calculator = p.flutter.buildFlutterApplication {
+              pname = "expidus-calculator";
+              version = "${shortVersion}+git-${shortRev}";
 
-          flutterBuildFlags = [
-            "--dart-define=COMMIT_HASH=${shortRev}"
-          ];
+              src = lib.cleanSource self;
 
-          depsListFile = ./deps.json;
-          vendorHash = "sha256-SJtfW+UZSmCugAgLd/O0PxtE53Fe/pouYKsg9fPv06o=";
+              flutterBuildFlags = [
+                "--dart-define=COMMIT_HASH=${shortRev}"
+              ];
 
-          postInstall = ''
-            rm $out/bin/calculator
-            ln -s $out/app/calculator $out/bin/expidus-calculator
+              pubspecLock = lib.importJSON ./pubspec.lock.json;
 
-            mkdir -p $out/share/applications
-            mv $out/app/data/com.expidusos.calculator.desktop $out/share/applications
+              gitHashes = {
+                libtokyo = "sha256-Zn30UmppXnzhs+t+EQNwAhaTPjCCxoN0a+AbH6bietg=";
+                libtokyo_flutter = "sha256-Zn30UmppXnzhs+t+EQNwAhaTPjCCxoN0a+AbH6bietg=";
+              };
 
-            mkdir -p $out/share/icons
-            mv $out/app/data/com.expidusos.calculator.png $out/share/icons
+              postInstall = ''
+                rm $out/bin/calculator
+                ln -s $out/app/calculator $out/bin/expidus-calculator
 
-            mkdir -p $out/share/metainfo
-            mv $out/app/data/com.expidusos.calculator.metainfo.xml $out/share/metainfo
+                mkdir -p $out/share/applications
+                mv $out/app/data/com.expidusos.calculator.desktop $out/share/applications
 
-            substituteInPlace "$out/share/applications/com.expidusos.calculator.desktop" \
-              --replace "Exec=calculator" "Exec=$out/bin/expidus-calculator" \
-              --replace "Icon=com.expidusos.calculator" "Icon=$out/share/icons/com.expidusos.calculator.png"
-          '';
+                mkdir -p $out/share/icons
+                mv $out/app/data/com.expidusos.calculator.png $out/share/icons
 
-          meta = {
-            description = "ExpidusOS Calculator";
-            homepage = "https://expidusos.com";
-            license = licenses.gpl3;
-            maintainers = with maintainers; [ RossComputerGuy ];
-            platforms = [ "x86_64-linux" "aarch64-linux" ];
+                mkdir -p $out/share/metainfo
+                mv $out/app/data/com.expidusos.calculator.metainfo.xml $out/share/metainfo
+
+                substituteInPlace "$out/share/applications/com.expidusos.calculator.desktop" \
+                  --replace "Exec=calculator" "Exec=$out/bin/expidus-calculator" \
+                  --replace "Icon=com.expidusos.calculator" "Icon=$out/share/icons/com.expidusos.calculator.png"
+              '';
+
+              meta = with lib; {
+                description = "ExpidusOS Calculator";
+                homepage = "https://expidusos.com";
+                license = licenses.gpl3;
+                maintainers = with maintainers; [ RossComputerGuy ];
+                platforms = [ "x86_64-linux" "aarch64-linux" ];
+              };
+            };
           };
         };
+      in {
+        legacyPackages = pkgs.appendOverlays [
+          (f: p: rec {
+            flutterPackages = p.recurseIntoAttrs (p.callPackages "${flutter-v322}/pkgs/development/compilers/flutter" {});
+            flutter = flutterPackages.stable;
+            flutter322 = flutterPackages.v3_22;
+          })
+          overlay
+        ];
 
-        devShells.default = pkgs.mkShell {
+        packages.default = self.legacyPackages.${system}.expidus.calculator;
+
+        devShells.default = self.legacyPackages.${system}.mkShell {
           inherit (self.packages.${system}.default) pname version name;
 
-          packages = with pkgs; [ flutter ];
+          inputsFrom = [ self.packages.${system}.default ];
+
+          packages = with self.legacyPackages.${system}; [ flutter yq ];
         };
       });
 }
